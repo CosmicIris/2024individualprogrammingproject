495869c0d30ab8c3c5f9988bb77f189d1986accb
CosmicIris <111626385+CosmicIris@users.noreply.github.com>
2024-05-11 19:40:50 -0600

    Update map.py

--- src/map.py
+++ src/map.py
@@ -1,5 +1,7 @@
 import time
-
+from wizardfight import wizardFight
+from cave import Cave
+c2 = Cave()
 
 class Map:
 
@@ -93,60 +95,7 @@ class Map:
                   "You must choose a spell. Please choose one from the list.")
 
       elif map_choice == "2":
-        while True:
-          time.sleep(1)
-          print(
-              "You travel to the cave. The cave is very dark, so you decide to\
-  open your spell book to create light. Which spell do you choose?")
-          time.sleep(1)
-          print("1) Flammae Raptura")
-          time.sleep(1)
-          print("2) Illuminus")
-          time.sleep(1)
-          print("3) Aqua Fluctus")
-          time.sleep(1)
-          print("4) Terrae")
-          time.sleep(1)
-          print("5) Ventus Fortis")
-          time.sleep(1)
-          light = input("Choose a spell: ")
-
-          if light == "2":
-            time.sleep(1)
-            print("You perform the spell titled “Illuminus”, which creates a\
-  spark of light at the tip of your wand. You can now see much better\
-            than before.")
-            print("You pick them up, putting them in your satchel. As you do\
-                 this, a dragon that is sleeping in the cave wakes up and notices to\
-                 trying to steal the jewels, breathing fire in an attempt to kill\
-                 you. Luckily, you notice the dragon just before it burns you to a\
-                 crisp. You remember your task and try to think of a way to distract the\
-                 dragon in order to quickly grab three of its eggs. How do you\
-                 distract the dragon?")
-
-          elif light == "1":
-            time.sleep(1)
-            print("You perform the spell titled “Flammae Raptura”, which\
- creates a burst of flame at the tip of your wand instead of pure light. Incorrect\
-               spell. Try again.")
-
-          elif light == "3":
-            time.sleep(1)
-            print("You perform the spell titled “Aqua Fluctus”, which creates\
-  an overwhelming amount of water and nearly floods the forest. Incorrect\
-                spell. Try again.")
-          elif light == "4":
-            time.sleep(1)
-            print("You perform the spell titled “Terrae”, which creates\
- many boulders, blocking your path. Incorrect spell. Try again.")
-          elif light == "5":
-            time.sleep(1)
-            print("You perform the spell titled “Ventus Fortis”, which\
-  creates creates a gust of wind. Incorrect spell. Try again.")
-          else:
-            time.sleep(1)
-            print("You must choose a spell. Please choose one from the list.")
-
+       c2.GoToCave()
       elif map_choice == "3":
         while True:
           time.sleep(1)
@@ -158,9 +107,9 @@ needed to break it. “Ah. I see. If you wish to acquire the unicorn horn, you\
   must fight me first.”')
           wizard_fight = input("Do you fight the wizard? Type 'y' to fight\
  the wizard: ")
-          if wizard_fight == "y":
-            time.sleep(1)
-            print(
+          if wizard_fight == "y": 
+              time.sleep(1)
+              print(
                 'You decide to fight the wizard. Instructions: The objective of\
   this fight will be to decrease the wizard’s health to 15. The wizard’s health\
   will start at 70. The wizard will start the fight by casting a certain spell. You\
@@ -169,15 +118,10 @@ needed to break it. “Ah. I see. If you wish to acquire the unicorn horn, you\
  not be as effective and the wizard could end up damaging your health more than you\
  damage his. If your health reaches 0, the fight will be over and you will have to\
  restart the fight. That is all for the instructions. Good luck.')
-            time.sleep(1)
-            wizard_health = 70
-            player_health = 75
-
-            
-          else:
-            time.sleep(1)
-            print("You have to fight the wizard if you want to break the\
-  curse. Please choose 'yes' to fight the wizard.")
+          time.sleep(1)
+          w1 = wizardFight()
+          w1.fight()   
+      
 
       elif map_choice == "4":
         time.sleep(1)
