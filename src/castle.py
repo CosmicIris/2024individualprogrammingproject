5b9a4025c67bcdf7e4111f14fd25e080a02b5be9
CosmicIris <111626385+CosmicIris@users.noreply.github.com>
2024-05-13 08:50:50 -0600

    Create castle.py

---
+++ src/castle.py
@@ -0,0 +1,13 @@
+import time
+class Castle:
+  def GoToCastle(self):
+    while True:
+      time.sleep(1)
+      print("You finally reach Esmeralda's castle. Upon entering, Esmeralda tells you\
+ that she's been expecting you. She tells you that she was aware you were chosen\
+ to save the kindgdom from the curse.'The final step in breaking my curse is to cast\
+ a spell using all the items you've collected. However, I will not give you\
+ the instructions for casting this spell freely. You must fight me for it, and if you\
+ win, I will give you the spell. If you lose, the curse will take over completely and\
+ I will become the new ruler. Good luck.'")
+      break
