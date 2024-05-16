9b812ac6e364058f5e48fb6e3ac02b346e8cb6db
CosmicIris <111626385+CosmicIris@users.noreply.github.com>
2024-05-15 22:28:21 -0600

    Update map.py

--- src/map.py
+++ src/map.py
@@ -1,17 +1,13 @@
 import time
-from cave import Cave
-from forest import Forest
-from hut import Hut
 
-c2 = Cave()
-f2 = Forest()
-h1 = Hut()
 
 class Map:
+
   def map_choice(self):
     while True:
       time.sleep(1)
-      print("Before you leave, the scribe tells you that you need to find ten items\
+      print(
+          "Before you leave, the scribe tells you that you need to find ten items\
  to break the spell: five eagle’s wings, three dragon eggs, one unicorn horn, and\
  snake venom. He also hands you a map with four locations on it: the forest, a cave, a\
  small hut, and Esmeralda’s castle, telling you that you must travel to each location\
