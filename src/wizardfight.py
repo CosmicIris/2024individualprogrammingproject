f4dee0f382d29329d3631a0fbed2dd6227e478db
CosmicIris <111626385+CosmicIris@users.noreply.github.com>
2024-05-15 22:11:53 -0600

    Update wizardfight.py

--- src/wizardfight.py
+++ src/wizardfight.py
@@ -10,18 +10,24 @@ class wizardFight:
 
    
   def wizardFight(self):
-    while True:
+    while self.wizardhealth > 15:
+      if self.playerhealth <= 15:
+        print("Your health is too low. Restarting the fight...")
+        self.playerhealth = 100  
+        self.enchanterhealth = 70
+        continue
+        
       print("Choose a spell to attack the wizard with from the list below:")
       time.sleep(1)
       print("1) Fireball: Creates a ball of fire that flies towards the target, dealing\
-  fire damage on impact and draining the opponent’s health by 7 points")
+ fire damage on impact and draining the opponent’s health by 7 points")
       time.sleep(1)
       print("2) Shadow Bolt: Unleashes a bolt of dark energy at the target, dealing\
-  necrotic damage and draining the opponent’s health by 15 points")
+ necrotic damage and draining the opponent’s health by 15 points")
       time.sleep(1)
       print("3) Ice Shards: Summons a sharp shard of ice to hurl at the target, causing\
-  cold damage and draining the opponent’s health by 10 points and slowing the\
-  opponent's movements")
+ cold damage and draining the opponent’s health by 10 points and slowing the\
+ opponent's movements")
       time.sleep(1)
      
       attack_spell_choice = input("Spell Choice: ")
@@ -37,16 +43,19 @@ class wizardFight:
           self.playerhealth -= 5
           print("The wizard's earthquake attack deals 5 damage to you.")
           print("Your health is now", self.playerhealth)
+          continue
         
         elif attack_name == "frost nova":
           self.playerhealth -= 7
           print("The wizard's frost nova attack deals 7 damage to you.")
           print("Your health is now", self.playerhealth)
+          continue
           
         elif attack_name == "shadow strike":
           self.playerhealth -= 10
         print("The wizard's frost nova attack deals 10 damage to you.")
         print("Your health is now", self.playerhealth)
+        continue
 
 
       
@@ -61,16 +70,19 @@ class wizardFight:
           self.playerhealth -= 5
           print("The wizard's earthquake attack deals 5 damage to you.")
           print("Your health is now", self.playerhealth)
+          continue
 
         elif attack_name == "frost nova":
           self.playerhealth -= 7
           print("The wizard's frost nova attack deals 7 damage to you.")
           print("Your health is now", self.playerhealth)
+          continue
 
         elif attack_name == "shadow strike":
           self.playerhealth -= 10
           print("The wizard's frost nova attack deals 10 damage to you.")
           print("Your health is now", self.playerhealth)
+          continue
 
 
       elif attack_spell_choice == "3":
@@ -84,11 +96,13 @@ class wizardFight:
           self.playerhealth -= 5
           print("The wizard's earthquake attack deals 5 damage to you.")
           print("Your health is now", self.playerhealth)
+          continue
 
         elif attack_name == "frost nova":
           self.playerhealth -= 7
           print("The wizard's frost nova attack deals 7 damage to you.")
           print("Your health is now", self.playerhealth)
+          continue
 
         elif attack_name == "shadow strike":
           self.playerhealth -= 10
@@ -99,10 +113,29 @@ class wizardFight:
           print("You have been defeated. Try again.")
           continue
         
-      if self.wizardhealth <= 15:
-          print("You defeat the wizard. As promised, he gives you the unicorn\
+    else:
+      print("You defeat the wizard. As promised, he gives you the unicorn\
   horn. The final place you must travel to is Esmeralda’s castle.")
-          break
+    time.sleep(1)
+    print("You finally reach Esmeralda's castle. Upon entering, Esmeralda tells\
+you that she's been expecting you. She tells you that she was aware you were chosen\
+  to save the kindgdom from the curse.'The final step in breaking my curse is\
+  to cast a spell using all the items you've collected. However, I will not give you\
+  the instructions for casting this spell freely. You must fight me for it, and if you\
+  win, I will give you the spell. If you lose, the curse will take over completely and\
+  I will become the new ruler.'")
+    time.sleep(1)
+    print("Instructions: Similarly to the fight with the wizard, the object of this\
+  fight will be to decrease Esmeralda's health to 15. The fight\
+will begin with you choosing a spell from the available options. Certain spells will\
+  deteriorate Esmeralda's health by a certain amount. However, Esmeralda's attacks\
+  could damage your health more than it damages her's. If your health reaches 15, the\
+  fight will be over and you will have to restart the fight. That is all for the\
+  instructions. Good luck. (P.S: If you thought that this would be similar to\
+  the wizard's fight and that it would be easy to defeat the enchanter, you are\
+  wrong. The enchanter is much more powerful than the wizard. Choose your spells\
+ wisely.)")
+      
     
        
           
