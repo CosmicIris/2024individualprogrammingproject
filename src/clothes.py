1829ff4861c4535b829c867b6e0537d13a7cbf17
CosmicIris <111626385+CosmicIris@users.noreply.github.com>
2024-05-15 22:07:10 -0600

    Update clothes.py

--- src/clothes.py
+++ src/clothes.py
@@ -8,7 +8,7 @@ class Clothes:
       time.sleep(1)
       print("What do you choose to change into?")
       time.sleep(1)
-      print("1) light, wool clothes with a cloak")
+      print("1) light clothes with a wool cloak")
       time.sleep(1)
       print("2) an itchy, heavy sweater with jeans")
       time.sleep(1)
@@ -18,18 +18,18 @@ class Clothes:
       
       if clothes == "1":
                   time.sleep(1)
-                  print('You choose to change into light, wool clothes that are\
- comfortable and warm, as well as a cloak to shield yourself from rain.')
+                  print('You choose to change into light clothes that are\
+ comfortable and warm, as well as a wool cloak to shield yourself from rain.')
                   break
       elif clothes == "2":
                   time.sleep(1)
                   print('After stepping out of your home, you realize that the sweater\
  is very uncomfortable and start furiously scratching yourself until\
-you bleed. Try again.')  
+ you bleed. Try again.')  
       elif clothes == "3":
                   time.sleep(1)
                   print('After stepping out of your home, you realize that choosing these\
- clothes was a terrible mistake, as it is pouring heavily and quickly catch\
+ clothes was a terrible mistake, as it is pouring heavily and you quickly catch\
  hypothermia due to the rain. You die within an hour. Try again. ')
       else:
                 time.sleep(1)
